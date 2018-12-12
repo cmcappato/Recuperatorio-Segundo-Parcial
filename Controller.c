699946b3cc74de16b1442b1ebbecf4e447f30f7a
cmcappato <40859051+cmcappato@users.noreply.github.com>
2018-12-12 12:14:22 -0300

    Add files via upload

--- Controller.c
+++ Controller.c
@@ -58,7 +58,116 @@ int controller_List(LinkedList* this)
     return indice;
 }
 
+int controller_listAbonos(LinkedList* this, LinkedList* this2)
+{
+    eAbono* auxiliarAbono;
+    eCliente* auxiliarCliente;
+    int indice = 0;
+    int largoListaDos = ll_len(this2);
+    int idAuxAbono;
+    int tipoAuxAbono;
+    int idClienteAuxAbono;
+    int importeFinalAuxAbono;
+    char nombreAuxCliente[50];
+    char sexoAuxCliente[2];
+    char numeroTelefonicoAuxCliente[21];
+    int importeAuxCliente, i, j;
 
+    if(this != NULL)
+    {
+        if(ll_len(this) >= 0)
+        {
+            printf(" Id\t\tTipo\t\tId Cliente\t Importe Total   \n");
+
+            for(i = 0; i < ll_len(this); i++)
+            {
+                auxiliarAbono = (eAbono*)ll_get(this, i);
+                getIdAbono(auxiliarAbono, &idAuxAbono);
+                getTipoAbono(auxiliarAbono, &tipoAuxAbono);
+                getIdAbonoCliente(auxiliarAbono, &idClienteAuxAbono);
+                getImporteFinalAbono(auxiliarAbono, &importeFinalAuxAbono);
+                printf("%3d\t\t  %d\t\t    %d\t\t    %d\t\t  \n", idAuxAbono, tipoAuxAbono, idClienteAuxAbono, importeFinalAuxAbono);
+
+                printf("llega3");
+
+                for( j = 1; j <largoListaDos ; j++)
+                {
+                    auxiliarCliente = (eCliente*)ll_get(this2, j);
+                    if(auxiliarAbono -> idCliente == auxiliarCliente -> id)
+                    {
+                        getNombreCliente(auxiliarCliente, nombreAuxCliente);
+                        getSexo(auxiliarCliente, sexoAuxCliente);
+                        getNumeroTelefonico(auxiliarCliente,numeroTelefonicoAuxCliente);
+                        getImporte(auxiliarCliente, &importeAuxCliente);
+                        printf("%-3s\t    %s\t     %-s\t      %d\t \n", nombreAuxCliente, sexoAuxCliente, numeroTelefonicoAuxCliente, importeAuxCliente );
+                    }
+                }
+            }
+
+            indice = 1;
+        }
+        else
+        {
+            printf("No se cargaron los datos.\n");
+        }
+
+    }
+    return indice;
+}
+
+int controller_saveAsText(char* path, LinkedList* pArrayAbono)
+{
+    int indice = 0;
+    int largoLista = ll_len(pArrayAbono);
+    eAbono* pArrayAbonoAux;
+    FILE* fp = fopen(path, "w");
+    int i;
+    if(fp==NULL)
+    {
+        printf("Error al abrir archivo para guardar\n");
+        return indice;
+    }
+    fprintf(fp, "Id,Tipo,Id Cliente,Importe final\n");
+    if(pArrayAbono!=NULL)
+    {
+        for(i = 0; i < largoLista; i++)
+        {
+            pArrayAbonoAux = (eAbono*)ll_get(pArrayAbono, i);
+            fprintf(fp, "%d,%d,%d,%f\n", pArrayAbonoAux->id, pArrayAbonoAux->tipo, pArrayAbonoAux->idCliente, pArrayAbonoAux->importeFinal);
+        }
+        printf("Se ha realizado el guardado del archivo con exito !\n");
+        indice = 1;
+    }
+    fclose(fp);
+    return indice;
+}
+
+
+int controller_saveAsBinary(char* path, LinkedList* pArrayAbono)
+{
+    int indice = 0;
+    int largoLista = ll_len(pArrayAbono);
+    eAbono* pAbonoAux;
+    FILE* fp = fopen(path, "wb");
+    int i;
+    if(fp==NULL)
+    {
+        printf("Error al guardar\n");
+        return indice;
+    }
+    if(pArrayAbono!=NULL)
+    {
+        for(i = 0; i < largoLista; i++)
+        {
+            pAbonoAux = (eAbono*)ll_get(pArrayAbono, i);
+            fwrite(pAbonoAux, sizeof(eAbono), 1, fp);
+        }
+        printf("Se ha realizado el guardado del archivo con exito !\n");
+        indice = 1;
+    }
+    fclose(fp);
+    return indice;
+}
 
 
 
--- main.c
+++ main.c
@@ -15,7 +15,7 @@ int main()
 
     do
     {
-        printf("***************************************************************************** \n");
+        printf("******************************************************************************* \n");
         printf("1- Cargar los datos de los clientes desde el archivo clientes.csv (modo texto) \n");
         printf("2- Listar los clientes cargados \n");
         printf("3- Crear nueva lista con abonos \n");
@@ -23,7 +23,7 @@ int main()
         printf("5- Guardar los datos de los abonos en el archivo abonos.csv (modo texto) \n");
         printf("6- Guardar los datos de los abonos en el archivo abonos.bin (modo binario) \n");
         printf("10- Salir \n");
-        printf("***************************************************************************** \n");
+        printf("******************************************************************************* \n");
         printf("Elija una opcion: \n");
         scanf("%d",&opcion);
 
@@ -41,21 +41,21 @@ int main()
                 printf("Lista creada con exito \n");
                 break;
             case 4:
+                ll_sort(listadoAbono, sortByImporteAbono, 0);
+                controller_listAbonos(listadoAbono, listadoClientes);
                 break;
             case 5:
+                controller_saveAsText("Abonos.csv", listadoAbono);
                 break;
             case 6:
+                controller_saveAsBinary("Abonos.bin", listadoAbono);
                 break;
             case 10:
+                printf("Gracias por utilizar el programa \n");
                 break;
             default:
                 printf("Error, ingrese una opcion del 1 al 10");
         }
-
-
-
-
-
         system("pause");
         system("cls");
     }while (opcion != 10);
